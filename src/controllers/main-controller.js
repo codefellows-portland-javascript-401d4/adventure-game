799ad022a798566e495abf26436163d98ab32e3f
Nathan P. Keene <npk@txtincorporated.com>
2016-11-30 16:45:57 -0800

    Add catchBigfoot fn

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -1,8 +1,9 @@
 export default function mainController() {
   const self = this;
   self.location = 'clearing';
-  self.crystal = false,
-  self.gameOver = false,
+  self.crystal = false;
+  self.sasquatchLocation = 'clearing';
+  self.gameOver = false;
   self.lookAround = function(location) {
     if(self.crystal) {
       return location.descriptionCrystal;
@@ -12,5 +13,12 @@ export default function mainController() {
   };  
   self.move = function(newLocation) {
     self.location = newLocation;
+  };
+  self.catchBigfoot = function() {
+    if (self.crystal) {
+      return 'You caught Bigfoot!  Fame and fortune will surely be yours.';
+    } else {
+      return 'Awww!  He ran right over you and got away.  Go home and tell all your friends.';
+    }
   };
 }
