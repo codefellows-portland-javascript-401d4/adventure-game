40b2b27b8397297f3974fab9be4e9fae0e8b7b81
DrewStock <andrewdstock@gmail.com>
2016-11-30 17:22:19 -0800

    basic logic in place

---
+++ src/controllers/day30.js
@@ -0,0 +1,16 @@
+day30Controller.$inject = ['$scope'];
+
+export default function day30Controller($scope) {
+  $scope.instructions = 'Many rivers to cross and you are at one now.';
+
+
+  $scope.ford = function() {
+    $scope.day = 'dead';
+    $scope.message = 'You done drowned!';
+  };
+
+  $scope.wait = function() {
+    $scope.day = 'win';
+    $scope.message = 'You made it to Oregon, congrats!';
+  };
+}
