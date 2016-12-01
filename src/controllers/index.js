15cd81e51104e3665772920f9b683e8a199c1dcd
Nathan P. Keene <npk@txtincorporated.com>
2016-11-30 16:29:00 -0800

    Add stream

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import mainController from './main-controller';
 import clearingController from './clearing-controller';
+import streamController from './stream-controller';
 
 const controllers = angular.module('controllers', []);
 
 controllers.controller('mainController', mainController);
 controllers.controller('clearingController', clearingController);
+controllers.controller('streamController', streamController);
 
 export default controllers.name;
--- src/controllers/stream-controller.js
+++ src/controllers/stream-controller.js
@@ -1,11 +1,12 @@
-export default function clearingController() {
-  // const self = this;
-  // self.name = 'clearing';
-  // self.initDescription = 'This is a clearing';
-  // self.descriptionCrystal = 'You have a crystal';
-  // self.descriptionNoCrystal = 'You are missing a crystal';
-  // self.movement = {
-  //   forward: 'stream'
-  // };
-  // self.items = [];
+export default function streamController() {
+  const self = this;
+  self.name = 'stream';
+  self.initDescription = 'This is a stream';
+  self.descriptionCrystal = 'You have a crystal.  Bigfootprints have appeared!';
+  self.descriptionNoCrystal = 'You are missing a crystal';
+  self.movement = {
+    forward: 'cave', 
+    backward: 'clearing'
+  };
+  self.items = [];
 }
