50b09f7c5ec573c3f42320eaa53b5e0d3f3e105e
DrewStock <andrewdstock@gmail.com>
2016-12-01 16:47:08 -0800

    ng-show working, added $scope.watchGroup

--- src/controllers/day30.js
+++ src/controllers/day30.js
@@ -4,13 +4,13 @@ export default function day30Controller($scope) {
   $scope.instructions = 'Many rivers to cross and you are at one now.';
 
 
-  $scope.ford = function() {
-    $scope.day = 'dead';
-    $scope.message = 'You done drowned!';
-  };
-
-  $scope.wait = function() {
-    $scope.day = 'win';
-    $scope.message = 'You made it to Oregon, congrats!';
-  };
+  // $scope.ford = function() {
+  //   $scope.day = 'dead';
+  //   $scope.message = 'You done drowned!';
+  // };
+  //
+  // $scope.wait = function() {
+  //   $scope.day = 'win';
+  //   $scope.message = 'You made it to Oregon, congrats!';
+  // };
 }
--- src/controllers/trail.js
+++ src/controllers/trail.js
@@ -1,11 +1,75 @@
 trailController.$inject = ['$scope'];
 
 export default function trailController($scope) {
-  $scope.message = 'You are on the trail.';
+  // $scope.start = true;
   $scope.day = '';
   $scope.supplies = {
     food : false,
     gun : false
   };
+
+  $scope.$watchGroup(['supplies.gun', 'supplies.food'], function() {
+    if($scope.supplies.gun === true || $scope.supplies.food === true) {
+      $scope.day = 'day01';
+    }
+
+  }, true);
+
+  $scope.fight = function() {
+    if($scope.supplies.gun === false) {
+      $scope.day = 'dead';
+      $scope.message = 'You are dead!';
+    } else {
+      $scope.day = 'day20';
+      $scope.message = 'Nice shooting Tex!';
+    }
+  };
+
+  $scope.run = function() {
+    if($scope.supplies.food === false) {
+      $scope.day = 'dead';
+      $scope.message = 'You starved to death!';
+    } else {
+      $scope.day = 'day20';
+      $scope.message = 'Nice running, yeller belly! Thems some good vittles!';
+    }
+  };
+
+  $scope.town = function() {
+    if($scope.supplies.gun === false) {
+      $scope.day = 'day30';
+      $scope.message = 'You bought food!';
+    } else {
+      $scope.day = 'dead';
+      $scope.message = 'You got shot in a showdown!';
+    }
+  };
+
+  $scope.continue = function() {
+    $scope.day = 'dead';
+    $scope.message = 'You are dead!';
+  };
+
+  $scope.hunt = function() {
+    if($scope.supplies.gun === false) {
+      $scope.day = 'dead';
+      $scope.message = 'Flattened by a herd of buffalo! See ya!';
+    } else {
+      $scope.day = 'day30';
+      $scope.message = 'You bagged yourself a bison!';
+    }
+  };
+
+  $scope.ford = function() {
+    $scope.day = 'dead';
+    $scope.message = 'You done drowned!';
+  };
+
+  $scope.wait = function() {
+    $scope.day = 'win';
+    $scope.message = 'You made it to Oregon, congrats!';
+  };
+
+
 
 }
