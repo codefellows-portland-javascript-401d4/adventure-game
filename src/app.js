530cb06c0e84f11d719311c7365310321da375ff
Albert Reel <waxhoya@gmail.com>
2016-12-02 13:24:06 -0800

    converted to objects with forms.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,9 @@ require('./main.css');
 var app = angular.module('primary', []);
 
 function getRandScore(minister) {
-    console.log(minister[0],'strikes');
     let min = 1;
-    let max = minister[1];
-    let rMax = minister[3];
+    let max = minister[0].def;
+    let rMax = minister[1].def;
     let score = Math.floor(Math.random() * (max - min + 1)) + min;
     let scoreR = Math.floor(Math.random() * (rMax - min + 1)) + min;
     return score-scoreR;
@@ -19,50 +18,44 @@ app.controller('playerInfo', ['$scope', function ($scope) {
     $scope.rivalNation = 'Shaftistan';
     $scope.playerScore = 0;
     $scope.count = 1000;
-    $scope.defMinister = ['Gandi', 2, 9, 10];
-    $scope.eduMinister = ['Ben Franklin', 5, 10, 16];
-    $scope.primeMinister = ['Honest Abe', 12, 12, 12];
-    $scope.defMinisterRival = ['Putin', 8, 9, 2];
-    $scope.eduMinisterRival = ['Xi Jinping', 9, 12, 7];
-    $scope.primeMinisterRival = ['Angela Merkel', 5, 8, 9];
-    $scope.ministers = [
-        ['Honest Abe', 10, 10, 10],
-        ['Xi Jinping', 9, 12, 7],
-        ['Honest Carl', 32, 32, 32],
-        ['Gandi', 2, 9, 10],
-        ['Putin', 10, 9, 2],
-        ['Catherine the Great', 10, 9, 5],
-        ['Angela Merkel', 5, 12, 9],
-        ['Ben Franklin', 5, 10, 16]
+    $scope.defMinister = {name: "Gandi", def: 10, edu: 1, poli: 1, src: './Gandi.jpg'};
+    $scope.eduMinister = {name: "Ben Franklin", def: 10, edu: 1, poli: 1, src: './ben.jpg'};
+    $scope.primeMinister = {name: "Abe Lincoln", def: 10, edu: 10, poli: 10, src: './abe.jpg'};
+    $scope.defMinisterRival = {name: "Putin", def: 10, edu: 1, poli: 1, src: './putin.jpg'};
+    $scope.eduMinisterRival =  {name: "Xi Jinping", def: 10, edu: 10, poli: 10, src: './XiJinping.jpg'};
+    $scope.primeMinisterRival = {name: "Angela Merkle", def: 10, edu: 1, poli: 1, src: './aMerkle.jpg'};
 
-        ];
-    
+    $scope.objMinisters = [
+        {name: "Abe Lincoln", def: 10, edu: 10, poli: 10, src: './abe.jpg'},
+        {name: "Xi Jinping", def: 10, edu: 10, poli: 10, src: './XiJinping.jpg'},
+        {name: "Gandi", def: 10, edu: 1, poli: 1, src: './Gandi.jpg'},
+        {name: "Angela Merkle", def: 10, edu: 1, poli: 1, src: './aMerkle.jpg'},
+        {name: "Catherine the Great", def: 10, edu: 1, poli: 1, src: './catherine.jpg'},
+        {name: "Putin", def: 10, edu: 1, poli: 1, src: './putin.jpg'},
+        {name: "Ben Franklin", def: 10, edu: 1, poli: 1, src: './ben.jpg'},
+        {name: "Albert", def: 10, edu: 1, poli: 1, src: './einstein.jpg'}
+    ];
+
     $scope.defMan = function() {
         $scope.playerScore += getRandScore([
-            $scope.defMinister[0],
-            $scope.defMinister[1],
-            $scope.defMinisterRival[0],
-            $scope.defMinisterRival[1]
+            $scope.defMinister,
+            $scope.defMinisterRival
         ]);
         $scope.count +=1;
     };
 
     $scope.makeLaw = function() {
         $scope.playerScore += getRandScore([
-            $scope.primeMinister[0],
-            $scope.primeMinister[1],
-            $scope.primeMinisterRival[0],
-            $scope.primeMinisterRival[1]
+            $scope.primeMinister,
+            $scope.primeMinisterRival
         ]);
         $scope.count +=1;
     };
 
     $scope.eduPop = function() {
         $scope.playerScore += getRandScore([
-            $scope.eduMinister[0],
-            $scope.eduMinister[1],
-            $scope.eduMinisterRival[0],
-            $scope.eduMinisterRival[1]
+            $scope.eduMinister,
+            $scope.eduMinisterRival
         ]);
         $scope.count +=1;
     };
