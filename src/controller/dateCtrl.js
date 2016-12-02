836cbacfae7cc58b8e74b41e1d84d973be5ce7ec
David Chase <dchasepdx@gmail.com>
2016-12-02 15:14:12 -0800

    tracking stage of the date

---
+++ src/controller/dateCtrl.js
@@ -0,0 +1,12 @@
+// import angular from 'angular';
+// import { profiles } from '../profiles';
+
+// export default function date($scope) {
+//   $scope.goodOption = () => {
+//     if ($scope.current.order.goodOption.response) {
+//       $scope.dateMessage
+//       score ++;
+
+//     }
+//   };
+// }
--- src/profiles.js
+++ src/profiles.js
@@ -6,7 +6,17 @@ profiles.chad = {
   message: 'I know the SICKEST bar, we should go!',
   yesResponse: 'OMG I LOVE THAT PLACE',
   noResponse: 'What is wrong with you?',
-  whoPays: 
+  date1: {
+    optionA: {
+      response: true,
+      description: 'chad loves beer'
+    },
+    optionB: {
+      response: false,
+      description: 'gross, water'
+    }
+
+  }
 };
 
 profiles.becky = {
@@ -24,9 +34,28 @@ profiles.dates = {
   }
 };
 
+const dates = {};
+
+dates.bar = {
+  location: 'bar',
+  text: 'you and your date sit down at the bar, the waiter asks what you want to drink',
+  stage1: {
+    optionA: 'order beer',
+    optionB: 'order water'
+  },
+  stage2: {
+    optionA: 'play pool',
+    optionB: 'have a heart to heart'
+  }
+};
+
 profiles.chad.next = profiles.becky;
 profiles.becky.next = null;
 
-export {profiles};
+dates.bar.stage1.next = dates.bar.stage2;
+
+
+
+export {profiles, dates};
 
   // img: '../src/img/chad-profile.jpg',
