354950635dc4975a2ffa2e5c3259acc48d6495e7
Albert Reel <waxhoya@gmail.com>
2016-12-01 20:37:23 -0800

    pushing MVP

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
 const angular = require('angular');
-require('./style.css');
+require('./main.css');
 
 var app = angular.module('primary', []);
 
-app.controller('playerInfo', function ($scope) {
-    $scope.userName;
-    $scope.nationName;
-})
+function getRandScore() {
+    let min = 1;
+    let max = 15;
+    let score = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log('This is the rand score', score);
+    return score;
+}
+
+app.controller('playerInfo', ['$scope', function ($scope) {
+    $scope.userName = '';
+    $scope.nationName = 'Cocomo';
+    $scope.rivalNation = 'Shaftistan';
+    $scope.playerScore = 0;
+    $scope.rivalScore = 0;
+
+
+    
+    $scope.defMan = function() {
+        $scope.playerScore += getRandScore();
+    }
+
+    $scope.makeLaw = function() {
+        $scope.playerScore += getRandScore();
+    }
+
+    $scope.eduPop = function() {
+        $scope.playerScore += getRandScore();
+    }
+
+
+}]);
