cf1a4f46b06e576114490f63857ef505dc9175bc
mizutombo <thomas.shultz52@gmail.com>
2016-12-04 16:53:45 -0800

    completed code for pantry ... needs review

--- src/controllers/game.js
+++ src/controllers/game.js
@@ -4,16 +4,16 @@ Controller.$inject = ['$scope'];
 
 export default function Controller($scope) {
   $scope.room = 'dining';
-  $scope.getIngredients = false;
   $scope.haveIngredients = false;
+  $scope.drankWine = false;
   $scope.risottoAssigned = false;
   $scope.risottoMade = false;
   $scope.office = function() { $scope.room = 'office'; };
   $scope.kitchen = function() { $scope.room = 'kitchen'; };
   $scope.pantry = function() { $scope.room = 'pantry'; };
   $scope.dining = function() { $scope.room = 'dining'; };
-  $scope.needIngredients = function() { $scope.getIngredients = true; };
   $scope.getIngredients = function() { $scope.haveIngredients = true; };
+  $scope.drinkWine = function() { $scope.drankWine = true; };
   $scope.makeRisotto = function() { $scope.risottoMade = true; };
   $scope.assignRisotto = function() { $scope.risottoAssigned = true; };
   $scope.win = function() { $scope.room = 'victory'; };
