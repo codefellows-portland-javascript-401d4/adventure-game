a419a06a5de16d8ff724e6bb7d64cbc1846defaa
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-05 14:19:25 -0800

    Deleted swipeCtrl (dead file)

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import game from './gameCtrl';
-import swipe from './swipeCtrl';
 import message from './messageCtrl';
 
 const module = angular.module('controllers', []);
 
 module.controller('game', game);
-module.controller('swipe', swipe);
 module.controller('message', message);
 
 export default module.name;
