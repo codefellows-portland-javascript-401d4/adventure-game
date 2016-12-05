547df7af1c1769a716b852fb92cf702516bee93f
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-05 14:22:47 -0800

    Deleted messageCtrl (dead file)

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,10 +1,8 @@
 import angular from 'angular';
 import game from './gameCtrl';
-import message from './messageCtrl';
 
 const module = angular.module('controllers', []);
 
 module.controller('game', game);
-module.controller('message', message);
 
 export default module.name;
