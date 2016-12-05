fac14106b1714636eb64434a3f6326c136e29889
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-05 14:18:28 -0800

    Removed dateCtrl (dead file)

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,13 +2,11 @@ import angular from 'angular';
 import game from './gameCtrl';
 import swipe from './swipeCtrl';
 import message from './messageCtrl';
-import date from './dateCtrl';
 
 const module = angular.module('controllers', []);
 
 module.controller('game', game);
 module.controller('swipe', swipe);
 module.controller('message', message);
-module.controller('date', date);
 
 export default module.name;
