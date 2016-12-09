3816afbcd5948369b69ee1c320f4b844fa6b5391
Nathan Pickard <nathan.pickard87@gmail.com>
2016-12-08 17:29:32 -0800

    Edited controllers, added css folder and adjusted package.json

--- app/src/controllers/index.js
+++ app/src/controllers/index.js
@@ -2,11 +2,13 @@ import angular from 'angular';
 import firstRoom from './firstRoom';
 import secondRoom from './secondRoom';
 import thirdRoom from './thirdRoom';
+import lastRoom from './lastRoom';
 
 const module = angular.module('controllers', []);
 
 module.controller('firstRoom', firstRoom);
 module.controller('secondRoom', secondRoom);
 module.controller('thirdRoom', thirdRoom);
+module.controller('lastRoom', lastRoom);
 
 export default module.name;
