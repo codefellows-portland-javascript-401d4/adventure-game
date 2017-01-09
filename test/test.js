0c48b724068e3ad827791a17abc5e3499a50eefe
mizutombo <thomas.shultz52@gmail.com>
2017-01-09 12:52:37 -0800

    added 7 more room-to-room navigation tests and validated passing tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const { assert } = chai;
 describe( 'counter controller', () => {
 
   beforeEach(angular.mock.module('controllers'));
-    
+
   let $controller, $scope;
 
   beforeEach(angular.mock.inject(function($rootScope, _$controller_) {
@@ -18,11 +18,67 @@ describe( 'counter controller', () => {
     assert.equal($scope.room, 'dining');
   });
 
-  it('room-to-room navigation works', () => {
+  it('room-to-room navigation works: dining to office', () => {
     $controller('game', { $scope });
     $scope.room = 'dining';
     $scope.office();
     assert.equal($scope.room, 'office');
+  });
+
+// new test
+  it('room-to-room navigation works: dining to kitchen', () => {
+    $controller('game', { $scope });
+    $scope.room = 'dining';
+    $scope.kitchen();
+    assert.equal($scope.room, 'kitchen');
+  });
+
+// new test
+  it('room-to-room navigation works: kitchen to dining', () => {
+    $controller('game', { $scope });
+    $scope.room = 'kitchen';
+    $scope.dining();
+    assert.equal($scope.room, 'dining');
+  });
+
+// new test
+  it('room-to-room navigation works: kitchen to pantry', () => {
+    $controller('game', { $scope });
+    $scope.room = 'kitchen';
+    $scope.pantry();
+    assert.equal($scope.room, 'pantry');
+  });
+
+// new test
+  it('room-to-room navigation works: pantry to kitchen', () => {
+    $controller('game', { $scope });
+    $scope.room = 'pantry';
+    $scope.kitchen();
+    assert.equal($scope.room, 'kitchen');
+  });
+
+// new test
+  it('room-to-room navigation works: office to dining', () => {
+    $controller('game', { $scope });
+    $scope.room = 'office';
+    $scope.dining();
+    assert.equal($scope.room, 'dining');
+  });
+
+// new test
+  it('room-to-room navigation works: office to lose', () => {
+    $controller('game', { $scope });
+    $scope.room = 'office';
+    $scope.lose();
+    assert.equal($scope.room, 'lose');
+  });
+
+// new test
+  it('room-to-room navigation works: office to victory', () => {
+    $controller('game', { $scope });
+    $scope.room = 'office';
+    $scope.victory();
+    assert.equal($scope.room, 'victory');
   });
 
   it('completed tasks change state', () => {
